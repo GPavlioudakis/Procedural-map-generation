154bd515998f0d99e01ceabb32d5942a470cff07
Giannis Pavlioudakis <gpavlioudakis@gmail.com>
2020-07-29 17:29:32 +0300

    Initial commit

---
+++ Scripts/MapDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
+    public void DrawTexture(Texture2D texture)
+    {
+        textureRenderer.enabled = true;
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+        meshRenderer.enabled = false;
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshRenderer.enabled = true;
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.enabled = false;
+    }
+}
---
+++ Scripts/MapGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode {NoiseMap,ColorMap, Mesh};
+
+    public DrawMode drawMode;
+
+    
+
+    public int mapWidth;
+    public int mapHeight;
+    public int seed;
+    public float noiseScale;
+    public float heightMultiplier;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+    public Vector2 offset;
+    public AnimationCurve heightCurve;
+
+    public TerrainType[] regions;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistance, lacunarity, offset);
+        Color[] colorMap = new Color[mapWidth * mapHeight]; 
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+
+                for (int i = 0; i < regions.Length; i++)
+                {
+
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].TerrainColor;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = GetComponent<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColorMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, heightMultiplier, heightCurve), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+    }
+
+    [System.Serializable]
+    public struct TerrainType 
+    {
+        public string TerrainName;
+        public float height;
+        public Color TerrainColor;
+    }
+
+    public void OnValidate()
+    {
+        if (lacunarity < 1)
+        {
+            lacunarity = 1.0f;
+        }
+
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        
+    }
+}
